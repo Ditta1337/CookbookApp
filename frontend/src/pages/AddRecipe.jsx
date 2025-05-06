555b0aa760e500f0cfa76c2bb6d460b17a9de621
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2025-05-06 18:32:03 +0200

    SCRUM-41 finished

--- frontend/src/pages/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe.jsx
@@ -2,56 +2,47 @@ import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const AddRecipe = () => {
-  // mock na potrzeby developmentu
-/*const recipeData = {
-  title: "Nalesniki",
-  description: "Przepyszne nalesniki z dzemem albo czymkolwiek innym co uwazasz za smaczne",
-  steps: [
-    {
-      title: "Zmieszaj skladniki",
-      description:
-        "Do jednej miski wsyp make, wbij jajka, wlej mleko, dodaj cukru do smaku i szczypte soli. Nastepnie wymieszaj skladniki przy pomocy miksera albo recznie",
-    },
-    { title: "Usmaz ciasto", description: "Dokladniejszy opis tego co ma byc zrobione" },
-    { title: "Wyloz na talerz", description: "Dokladniejszy opis tego co ma byc zrobione" },
-    { title: "Posmaruj dzemem i zawin", description: "Dokladniejszy opis tego co ma byc zrobione" },
-  ],
-  ingredients: [
-    {
-      name: "Maka",
-      quantity: 200,
-      unit: "g",
-    },
-    {
-      name: "Mleko",
-      quantity: 300,
-      unit: "ml",
-    },
-    {
-      name: "Jajko",
-      quantity: 2,
-      unit: "szt",
-    },
-  ],
-};*/
+  const recipeId = 0;
+  const [stepId,setStepId] = React.useState(1);
+  const [ingredientId,setIngredientId] = React.useState(1);
+  const [tagId,setTagId] = React.useState(1);
+  const getDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
   const [recipeData, setRecipeData] = React.useState({
-    recipeTitle: '',
-    recipeDescription: '',
+    id: recipeId,
+    name: '',
+    description: '',
+    date: getDate(),
   });
+  //tagi pewnie będą w bazie danych, ale na razie nie ma żadnego selecta tylko input
+  const [recipeTags, setRecipeTags] = React.useState([{
+    id: 0,
+    name: '',
+  }]);
+
   const [recipeSteps, setRecipeSteps] = React.useState([{
-    id: Date.now(),
+    id: 0,
     title: '',
     description: '',
   }]);
   const [recipeIngredients, setRecipeIngredients] = React.useState([{
-    id: Date.now(),
+    id: 0,
     name: '',
     quantity: 0,
     unit: '',
   }]);
   console.log(recipeData);
   console.log(recipeSteps);
+  console.log(recipeTags);
   console.log(recipeIngredients);
+  const fetchRecipeId = async() => {
+  };
+
   const handleInput = event => {
     const { name, value } = event.target;
     setRecipeData({
@@ -71,31 +62,59 @@ const AddRecipe = () => {
     newIngredients[i][name] = value;
     setRecipeIngredients(newIngredients);
   }
+  const handleTagChange = (event,i) => {
+    const { name, value } = event.target;
+    let newTags = [...recipeTags];
+    newTags[i][name] = value;
+    setRecipeTags(newTags);
+  }
   const handleAddStep = () => {
-    setRecipeSteps([...recipeSteps, { id: Date.now(), title: '', description: '' }]);
+    setRecipeSteps([...recipeSteps, { id: stepId, title: '', description: '' }]);
+    setStepId(stepId + 1);
+  }
+  const handleAddTag = () => {
+    setRecipeTags([...recipeTags, { id: tagId, name: '' }]);
+    setTagId(tagId + 1);
   }
 
   const handleAddIngredient = () => {
-    setRecipeIngredients([...recipeIngredients, { id: Date.now(), name: '', quantity: 0, unit: '' }]);
+    setRecipeIngredients([...recipeIngredients, { id: ingredientId, name: '', quantity: 0, unit: '' }]);
+    setIngredientId(ingredientId + 1);
+  }
+  const handleDeleteSteps = (i) => {
+    let deleteSteps = [...recipeSteps];
+    deleteSteps.splice(i, 1);
+    setRecipeSteps(deleteSteps);
+  }
+  const handleDeleteTags = (i) => {
+    let deleteTags = [...recipeTags];
+    deleteTags.splice(i, 1);
+    setRecipeTags(deleteTags);
+  }
+  const handleDeleteIngredients = (i) => {
+    let deleteIngredients = [...recipeIngredients];
+    deleteIngredients.splice(i, 1);
+    setRecipeIngredients(deleteIngredients);
   }
   const handleSubmit = (event) => {
     event.preventDefault();
-    const recipe = {
-      ...recipeData,
+    const json = {
+      recipe: recipeData,
+      tags: recipeTags,
       steps: recipeSteps,
       ingredients: recipeIngredients,
     };
     console.log(recipe);
     // tutaj dodaj kod do wyslania przepisu do backendu
-    fetch('http://localhost:8080/recipes', {
+    fetch('http://localhost:8080/recipes/post', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(recipe),
+      body: JSON.stringify(json),
     })
+    console.log(json);
   }
-
 
   return(
     <div>
@@ -106,20 +125,41 @@ const AddRecipe = () => {
             <legend className='text-5xl font-semibold mb-2'>Dodaj Przepis</legend>
             <label className="text-2xl font-semibold">Nazwa przepisu:</label>
             <input className='border-2 border-gray-300 rounded-md py-2 px-1 my-4' 
-            id="recipeTitle" 
-            name="recipeTitle" 
+            id="name" 
+            name="name" 
             type='text' 
-            placeholder='Nazwe przepisu'
+            placeholder='Nazwa przepisu'
             onChange={handleInput} 
             required />
             <label className="text-2xl font-semibold">Opis przepisu:</label>
-            <input className='border-2 border-gray-300 rounded-md py-2 px-1 my-4' 
-            id="recipeDescription" 
-            name="recipeDescription" 
+            <textarea className='border-2 border-gray-300 rounded-md py-2 px-1 my-4' 
+            id="description" 
+            name="description" 
             type='text' 
             placeholder='Opis przepisu'
             onChange={handleInput} 
+            rows={5}
             required />
+            <label className="text-2xl font-semibold">Tagi:</label>
+            {recipeTags.map((tag, i) => (
+                <div key={tag.id} className='flex flex-col border-gray-300 border-2 rounded-md py-2 my-4'>
+                <input className='border-2 border-gray-300 rounded-md py-2 px-1 mt-2 mb-1 mx-2'
+                id='name'
+                name='name'
+                type='text'
+                placeholder='Nazwa tagu'
+                onChange={event => handleTagChange(event,i)}
+                required></input>
+                <button className='bg-red-500 hover:bg-red-700 text-white font-bold my-2 py-2 px-4 rounded'
+                type='button'
+                onClick={() => handleDeleteTags(i)}>Usuń tag</button>
+                </div>
+              ))}
+              <div>
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded'
+              type='button'
+              onClick={handleAddTag}>Dodaj tag</button>
+            </div>
             <label className="text-2xl font-semibold">Składniki:</label>
             {recipeIngredients.map((ingredient, i) => (
                 <div key={ingredient.id} className='flex flex-col border-gray-300 border-2 rounded-md py-2 my-4'>
@@ -128,19 +168,25 @@ const AddRecipe = () => {
                 name='name'
                 type='text'
                 placeholder='Nazwa składnika'
-                onChange={event => handleIngredientChange(event,i)}></input>
+                onChange={event => handleIngredientChange(event,i)}
+                required></input>
                 <input className='border-2 border-gray-300 rounded-md py-2 px-1 mt-2 mb-1 mx-2'
                 id='quantity'
                 name='quantity'
                 type='text'
                 placeholder='Ilość'
-                onChange={event => handleIngredientChange(event,i)}></input>
+                onChange={event => handleIngredientChange(event,i)}
+                required></input>
                 <input className='border-2 border-gray-300 rounded-md py-2 px-1 mt-2 mb-1 mx-2'
                 id='unit'
                 name='unit'
                 type='text'
                 placeholder='Jednostka'
-                onChange={event => handleIngredientChange(event,i)}></input>
+                onChange={event => handleIngredientChange(event,i)}
+                required></input>
+                <button className='bg-red-500 hover:bg-red-700 text-white font-bold my-2 py-2 px-4 rounded'
+                type='button'
+                onClick={() => handleDeleteIngredients(i)}>Usuń składnik</button>
                 </div>
               ))}
               <div>
@@ -148,22 +194,28 @@ const AddRecipe = () => {
               type='button'
               onClick={handleAddIngredient}>Dodaj składnik</button>
             </div>
+
             <label className="text-2xl font-semibold">Kroki:</label>
             {recipeSteps.map((step, i) => (
-                <div key={step.id} className='flex flex-col'>
-                <input className='border-2 border-gray-300 rounded-md py-2 px-1 mt-2 mb-1'
+                <div key={step.id} className='flex flex-col border-gray-300 border-2 rounded-md py-2 my-4'>
+                <input className='border-2 border-gray-300 rounded-md mx-2 py-2 px-1 mt-2 mb-2'
                 id='title'
                 name='title'
                 type='text'
                 placeholder='Tytuł kroku'
-                onChange={event => handleStepChange(event,i)}></input>
-                <textarea className='border-2 border-gray-300 rounded-md py-2 px-1 mb-2'
+                onChange={event => handleStepChange(event,i)}
+                required></input>
+                <textarea className='border-2 border-gray-300 rounded-md  mx-2 py-2 px-1 mb-2'
                 id='description'
                 name='description'
                 type='text'
                 rows={5}
                 placeholder='Opis kroku'
-                onChange={event => handleStepChange(event,i)}></textarea>
+                onChange={event => handleStepChange(event,i)}
+                required></textarea>
+                <button className='bg-red-500 hover:bg-red-700 text-white font-bold my-2 py-2 px-4 rounded'
+                type='button'
+                onClick={() => handleDeleteSteps(i)}>Usuń krok</button>
                 </div>
               ))}
               <div>
