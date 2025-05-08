c76c5c293cacd234462bf99e67f8796905aacd28
Michał Dobranowski <mdbrnowski@gmail.com>
2025-05-08 21:57:06 +0200

    fixup! SCRUM-34

--- backend/schemas.py
+++ backend/schemas.py
@@ -8,7 +8,7 @@ class RecipeCreate(BaseModel):
     title: str
     description: str
     date: date
-    img:str
+    img: str
     tags: List[str]
     steps: List["StepCreate"]
     ingredients: List["RecipeIngredientCreate"]
@@ -17,6 +17,7 @@ class RecipeUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
     date: date
+    img: Optional[str] = None
     tags: Optional[List[str]] = None
     steps: Optional[List["StepCreate"]] = None
     ingredients: Optional[List["RecipeIngredientCreate"]] = None
