af83eec037732c3fc3ad98d760add0ee5f04e3f8
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2025-05-08 19:18:33 +0200

    Added recipe searching in VIewRecipes

--- frontend/src/pages/ViewRecipes.jsx
+++ frontend/src/pages/ViewRecipes.jsx
@@ -2,12 +2,31 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 const ViewRecipes = () => {
+  const recipeIds = ['Naleśniki', 'Jabłka prażone', 'Makaron z serem', 'Ziemniaki z koperkiem',
+     'Pizza', 'Ziemniaki z sosem czosnkowym', 'Prażynki', 'Paluszki rybne']; 
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [filteredRecipes, setFilteredRecipes] = React.useState(recipeIds);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const term = event.target.value;
+    setSearchTerm(term);
+    setFilteredRecipes(
+      recipeIds.filter((title) => title.includes(term))
+    );
+  }
+
   return (
     <div>
       <Navbar />
+      <input 
+      className="py-2 px-2 my-4 mx-2 font-semibold border-gray-300 border-2 rounded-md" 
+      value={searchTerm}
+      onChange={handleSearch} 
+      placeholder="Szukaj przepisu..." />
       <div style={{ display: "flex", flexDirection: "column" }}>
-        {[1, 2, 3, 4, 5, 6, 7].map((recipeId) => (
-          <a key={recipeId} href={`/recipes/${recipeId}`}>Przepis o id {recipeId}</a>
+        {filteredRecipes.map((title, i) => (
+          <a key={i} href={`/recipes/${i}`}>{title}</a>
         ))}
       </div>
     </div>
